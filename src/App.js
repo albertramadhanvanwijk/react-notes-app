32a24c1542668a2be029e06b8516f79b9a54fe72
Albert Ramadhan Van Wijk <albertramadhanvanwijk@gmail.com>
2021-09-02 15:23:56 +0700

    add new notes components

--- src/App.js
+++ src/App.js
@@ -15,15 +15,36 @@ const App = () => {
       date: "09/09/2001"
     },
     {
+			id: nanoid(),
+			text: 'This is my third note!',
+			date: '28/04/2021',
+		},
+    {
       id: nanoid(),
       text: "This is my new note!",
       date: "16/09/2001"
     },
   ]);
 
-  return <div className="container">
-    <NotesList notes={notes}/>
-  </div>;
+  const addNote = (text) => {
+		const date = new Date();
+		const newNote = {
+			id: nanoid(),
+			text: text,
+			date: date.toLocaleDateString(),
+		};
+		const newNotes = [...notes, newNote];
+		setNotes(newNotes);
+	};
+
+  return (
+    <div className="container">
+      <NotesList 
+      notes={notes} 
+      handleAddNote={addNote}
+      />
+    </div>
+  );
 };
 
 export default App;
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,11 +1,16 @@
 import Note from './Note';
+import AddNotes from './AddNotes'
 
-const NotesList = ({ notes }) => {
+const NotesList = ({ notes, handleAddNote }) => {
     return (
         <div className="notes-list">
             {notes.map((note) => 
-                <Note id={note.id} text={note.text} date={note.date} />
+                <Note id={note.id} 
+                text={note.text} 
+                date={note.date} 
+                />
             )}
+            <AddNotes handleAddNote={handleAddNote} />
         </div>
     );
   };
