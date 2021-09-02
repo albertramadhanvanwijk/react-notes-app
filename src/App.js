8edd93436c02db91017cd1e1dfcd122b6307f2a3
Albert Ramadhan Van Wijk <albertramadhanvanwijk@gmail.com>
2021-09-02 19:38:27 +0700

    fix new notes & themes

--- src/App.js
+++ src/App.js
@@ -4,50 +4,67 @@ import NotesList from './components/NotesList';
 import Search from './components/Search';
 import Header from './components/Header';
 
+const initialData = [
+	{
+		id: nanoid(),
+		text: 'This is my first note!',
+		date: '15/04/2021',
+	},
+	{
+		id: nanoid(),
+		text: 'This is my second note!',
+		date: '21/04/2021',
+	},
+	{
+		id: nanoid(),
+		text: 'This is my third note!',
+		date: '28/04/2021',
+	},
+	{
+		id: nanoid(),
+		text: 'This is my new note!',
+		date: '30/04/2021',
+	},
+]
+
 const App = () => {
-	const [notes, setNotes] = useState([
-		{
-			id: nanoid(),
-			text: 'This is my first note!',
-			date: '15/04/2021',
-		},
-		{
-			id: nanoid(),
-			text: 'This is my second note!',
-			date: '21/04/2021',
-		},
-		{
-			id: nanoid(),
-			text: 'This is my third note!',
-			date: '28/04/2021',
-		},
-		{
-			id: nanoid(),
-			text: 'This is my new note!',
-			date: '30/04/2021',
-		},
-	]);
+	const [notes, setNotes] = useState([]);
 
 	const [searchText, setSearchText] = useState('');
 
-	const [darkMode, setDarkMode] = useState(false);
+	const [darkMode, setDarkMode] = useState([]);
 
 	useEffect(() => {
+		const savedTheme = JSON.parse(
+    localStorage.getItem('react-note-theme-data')
+    );
+		
 		const savedNotes = JSON.parse(
 			localStorage.getItem('react-notes-app-data')
 		);
 
+    console.log(savedTheme)
+
+    if (savedTheme)  {             
+      setDarkMode(savedTheme)      
+    } else {             
+      setDarkMode(false)          
+    }
+
 		if (savedNotes) {
 			setNotes(savedNotes);
+		} else {
+			setNotes(initialData)
 		}
 	}, []);
 
-	useEffect(() => {
+	const saveNewNotesData = (data) => {
+		setNotes(data);
 		localStorage.setItem(
 			'react-notes-app-data',
-			JSON.stringify(notes)
+			JSON.stringify(data)
 		);
-	}, [notes]);
+	}
 
 	const addNote = (text) => {
 		const date = new Date();
@@ -57,18 +74,18 @@ const App = () => {
 			date: date.toLocaleDateString(),
 		};
 		const newNotes = [...notes, newNote];
-		setNotes(newNotes);
+		saveNewNotesData(newNotes);
 	};
 
 	const deleteNote = (id) => {
 		const newNotes = notes.filter((note) => note.id !== id);
-		setNotes(newNotes);
+		saveNewNotesData(newNotes);
 	};
 
 	return (
 		<div className={`${darkMode && 'dark-mode'}`}>
 			<div className='container'>
-				<Header handleToggleDarkMode={setDarkMode} />
+				<Header handleToggleDarkMode={setDarkMode} darkMode={darkMode} />
 				<Search handleSearchNote={setSearchText} />
 				<NotesList
 					notes={notes.filter((note) =>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,19 @@
 import React from 'react';
 
-const Header = ({ handleToggleDarkMode }) => {
-	return (
-		<div className='header'>
-			<h1>Catatanmu</h1>
-			<button
-				onClick={() =>
-					handleToggleDarkMode(
-						(previousDarkMode) => !previousDarkMode
-					)
-				}
-				className='save'
-			>
-				<img src="https://img.icons8.com/material-outlined/24/000000/quick-mode-on--v1.png"/>
-			</button>
-		</div>
-	);
+const Header = ({ handleToggleDarkMode,darkMode }) => {
+
+    const handleDarkMode = () => {
+        handleToggleDarkMode(!darkMode)         
+		localStorage.setItem('react-note-theme-data',!darkMode)
+    }
+    return (
+        <div className='header'>
+            <h1>Catatanmu</h1>
+            <button onClick={handleDarkMode} className='save'>
+                <img src="https://img.icons8.com/material-outlined/24/000000/quick-mode-on--v1.png" />
+            </button>
+        </div>
+    );
 };
 
 export default Header;
